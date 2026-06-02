d6476d0f99552e24cde20e24031d6a005c109198
dahiyakapil <kapildahiya309@gmail.com>
2026-06-02 15:05:32 +0530

    Express JS HTTP Method
    get
    post
    put
    delete

--- 2June/express.js
+++ 2June/express.js
@@ -2,4 +2,27 @@ const express = require("express");
 
 const app = express();
 
-console.log(app);
+// console.log(app); // Returns Application Object
+
+// HTTP Methods
+
+app.get("/users", (req, res) => {
+    res.send("Fetched Users : 200");
+})
+
+app.post("/users/create-user", (req, res) => {
+    res.send("Created user : 201");
+})
+
+app.put("/users/update", (req, res) => {
+    res.send("Updated user : Status Code : 200");
+})
+
+app.delete("/users/delete", (req, res) => {
+    res.send("User Deleted : 200");
+})
+
+
+app.listen(3000, (req, res) => {
+    console.log("Sever running on Port : 3000" )
+})
