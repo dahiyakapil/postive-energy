23d722af8a134bab07b86717f936b59312b30ec8
dahiyakapil <kapildahiya309@gmail.com>
2026-06-04 09:53:23 +0530

    Error Handler Middleware

--- 3June/mvc-practice/controller/userController.js
+++ 3June/mvc-practice/controller/userController.js
@@ -1,10 +1,15 @@
 const getUsers = (req, res) => {
-    res.status(200).json({
-        message: "Users Fetched Successfully"
-    })
+    // res.status(200).json({
+    //     message: "Users Fetched Successfully"
+    // })
+
+    throw new Error("NOT GOT USES");
+
 }
 
 const createUser = (req, res) => {
+
+
     res.status(201).json({
         message: "User Created Successfully",
         id:1,
--- 3June/mvc-practice/server.js
+++ 3June/mvc-practice/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express(); // It will return the Application Object
 const userRoutes = require("./routes/userRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 app.use(express.json());
 
@@ -12,6 +13,9 @@ const logger = (req, res, next) => {
 
 app.use(logger);
 app.use("/users", userRoutes);
+
+// Error Handler will always be placed afer routes
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Server is running on port : 3000");
